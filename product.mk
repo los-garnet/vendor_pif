6b7374d74c55ce6ef8d405a497d526bd7cd6ecb9
Ramadhani <ramadhanifals@gmail.com>
2025-01-20 00:53:23 +0700

    pif: Adapt and update PIF
    Change-Id: I8ceb48a1eb094e8067257c39e5ff270465e8fdae

--- product.mk
+++ product.mk
@@ -1,6 +1,6 @@
 # RRO Overlays
 PRODUCT_COPY_FILES += \
-    vendor/extra/config-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/overlay/config/config.xml
+    vendor/pif/config-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/overlay/config/config.xml
 
 PRODUCT_PACKAGES += \
     CertifiedPropsOverlay
